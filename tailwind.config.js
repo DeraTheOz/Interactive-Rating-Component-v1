0f3dafccced287130441ada4ea15892d46651af7
MannexDera <emmanuelchi545@gmail.com>
2025-04-30 23:36:35 +0100

    add color and font themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                white: 'hsl(0, 0%, 100%)',
+                orange: '#fc7614',
+                'light-grey': 'hsl(217, 12%, 63%)',
+                'medium-grey': 'hsl(216, 12%, 54%)',
+                'dark-blue': 'hsl(213, 19%, 18%)',
+                'very-dark-blue': 'hsl(216, 12%, 8%)',
+                'very-dark-blue-gradient': 'hsla(216, 12%, 8%, 0.2)'
+            },
+            fontFamily: {
+                overpass: ['Overpass', 'sans-serif']
+            }
+        }
+    },
+    plugins: []
+};
